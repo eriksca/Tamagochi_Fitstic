6cff11d7b9a3e254d10fdcf91c6f9f3f7eac2355
eriksca <73497621+eriksca@users.noreply.github.com>
2022-01-16 15:13:19 +0100

    Script instantiate food

--- New Unity Project/Assets/FoodSpawner.cs
+++ New Unity Project/Assets/FoodSpawner.cs
@@ -7,39 +7,56 @@ public class FoodSpawner : MonoBehaviour
 {
     [SerializeField] List<GameObject> foodPrefabs;
     [SerializeField] int maxPrefabs;
-    //GameManager gManager;
+    [SerializeField] int coroutineRepeatRate;
+    bool stopInstantiate;
+
     int prefabCounter = 0;
 
 
     private void Start()
     {
         StartCoroutine(SpawnFood());
+        stopInstantiate = false;
+    }
+
+    private void Update()
+    {
+        if (prefabCounter == foodPrefabs.Count)
+        {
+            prefabCounter = 0;
+        }
+        if (stopInstantiate)
+        {
+            StopCoroutine(SpawnFood());
+            Debug.LogWarning("Stop Coroutine");
+            stopInstantiate = false;
+
+        }
+        
     }
     public IEnumerator SpawnFood()
     {
-        Debug.Log("CIAO");
-
-        
-            Debug.Log("Porcodddio");
-            for (int i = 0; i < maxPrefabs; i++)
-            {
+        Debug.Log("Start to instantiate food prefabs");
+  
+          for (int i = 0; i < maxPrefabs; i++)
+          {
                 Vector3 spawnPos = new Vector3(Random.Range(-450, 450), 1, Random.Range(-450, 450));
-                if (prefabCounter < foodPrefabs.Count)
-                {
-                    prefabCounter++;
-
-                }
-                else if(prefabCounter==foodPrefabs.Count-1)
-                {
-                    prefabCounter = 0;
-                Debug.LogError("Dio santo");
-                }
+               
                 GameObject prefabToPlace = Instantiate(foodPrefabs[prefabCounter], spawnPos, Quaternion.identity);
                 
                 
                 Debug.LogWarning(prefabCounter);
-                yield return new WaitForSeconds(2f);
+            if (prefabCounter < foodPrefabs.Count)
+            {
+                prefabCounter++;
+
             }
+            
+            yield return new WaitForSeconds(coroutineRepeatRate);
+            
+          }
+        stopInstantiate = true;
+       
 
         
 
